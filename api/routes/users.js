1272dcaced69f4e38f8328338c3a60501ac6a941
flintco <connor.flint16@gmail.com>
2021-04-20 23:51:22 -0500

    Added 2nd API call in client

--- api/routes/users.js
+++ api/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('API Response 2');
 });
 
 module.exports = router;
--- client/src/App.js
+++ client/src/App.js
@@ -6,18 +6,27 @@ import React from 'react';
 class App extends React.Component{
   constructor(props){
     super(props);
-    this.state = {apiResponse: ""};
+    this.state = {
+      apiResponse1: "",
+      apiResponse2: ""
+    };
   }
 
-  callAPI(){
+  callAPI1(){
     fetch("http://localhost:9000/testAPI")
       .then(res => res.text())
-      .then(res => this.setState({apiResponse: res}));
+      .then(res => this.setState({apiResponse1: res}));
+  }
 
+  callAPI2(){
+    fetch("http://localhost:9000/users")
+      .then(res => res.text())
+      .then(res => this.setState({apiResponse2: res}));
   }
 
   componentWillMount(){
-    this.callAPI();
+    this.callAPI1();
+    this.callAPI2();
   }
 
   render(){
@@ -26,7 +35,8 @@ class App extends React.Component{
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        <p>{this.state.apiResponse}</p>
+        <p>{this.state.apiResponse1}</p>
+        <p>{this.state.apiResponse2}</p>
       </div>
     );
   }
