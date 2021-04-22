24b9b1c71401790e6b2c2f112f6ff2f5e5a73af9
flintco <connor.flint16@gmail.com>
2021-04-21 19:33:11 -0500

    Receive POST request with API

--- api/routes/index.js
+++ api/routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// POST method route
+router.post('/', function (req, res) {
+  res.send('POST request to the homepage')
+})
+
+
 module.exports = router;
