3ea0602b76c177a6bf9261e87dc486689fde8e5a
flintco <connor.flint16@gmail.com>
2021-04-25 18:12:17 -0500

    Added Mysql node module

--- api/database.js
+++ api/database.js
@@ -1,4 +1,21 @@
-console.log("Got here");
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'me',
+  password : 'secret',
+  database : 'my_db'
+});
+ 
+connection.connect();
+ 
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+ 
+connection.end();
+
+/*console.log("Got here");
 
 var mysql = require('mysql');
 
@@ -13,4 +30,4 @@ con.connect(function(err) {
   console.log("Database is connected!");
 });
 
-module.exports = con;
+module.exports = con;*/
