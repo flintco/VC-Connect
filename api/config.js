d698e6e0f4d043d28df7976427205e1cd36d45c6
flintco <connor.flint16@gmail.com>
2021-04-29 16:26:08 -0500

    Created database config file

---
+++ api/config.js
@@ -0,0 +1,10 @@
+//Run "Flush Priveledges" script in MySQL Workbench
+//Type command "node database.js" to start database
+var config = {
+  host     : 'localhost',
+  user     : 'root',
+  password : 'password',
+  database : 'schema_test'
+};
+
+module.exports = config;
