820b21e5c1086401c3fe98f3c2ab5365d9ec9b07
flintco <connor.flint16@gmail.com>
2021-04-30 17:38:21 -0500

    Got database.js to add to database

--- api/database.js
+++ api/database.js
@@ -14,5 +14,11 @@ connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
   if (error) throw error;
   console.log('The solution is: ', results[0].solution);
 });
+
+var sql = "INSERT INTO table1 (count) VALUES ('800')";
+connection.query(sql, function (err, result) {
+  if (err) throw err;
+  console.log("1 record inserted");
+});
  
 connection.end();
--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,8 +1,18 @@
 var express = require("express");
 var router = express.Router();
+let config = require('../config.js');
 
 router.get("/",function(req,res,next){
     res.send("API Response 1");
+
+    let connection = mysql.createConnection(config);
+
+    var sql = "INSERT INTO table1 (count) VALUES ('1000')";
+    connection.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+    });
+
 });
 
 module.exports = router;
