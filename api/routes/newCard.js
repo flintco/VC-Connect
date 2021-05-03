d79384feefa8c0ebc3e165dc09fa5547e2fffc0d
flintco <connor.flint16@gmail.com>
2021-05-03 13:17:45 -0500

    API adds question to db on newCard Post request

--- api/routes/newCard.js
+++ api/routes/newCard.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+let config = require('../config.js');
 
 // POST method route to add new card to database
 router.post('/', function (req, res) {
-    res.send('POST request for new card')
+    //res.send('POST request for new card')
+
+    let connection = mysql.createConnection(config);
+
+    var sql = "INSERT INTO table1 (COUNT, Question, Answer) VALUES (0, 'New item', 'This is the answer')";
+    connection.query(sql, function (err, result) {
+    if (err) throw err;
+        res.send('Question Successfully added');
+    });
 })
 
 module.exports = router;
